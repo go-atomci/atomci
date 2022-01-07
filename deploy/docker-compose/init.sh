ab23f494c2047143ca65fb0461b5f4e19569a59e
colynn.liu <colynn.liu@gmail.com>
2022-01-07 16:59:22 +0800

    deploy: add db migrate for registry change

--- deploy/docker-compose/init.sh
+++ deploy/docker-compose/init.sh
@@ -30,5 +30,16 @@ then
     echo "$NOT_UP restart failed, please check docker log use 'docker-compose logs [docker-name]'"
 fi
 
+MYSQL_DB=$(grep "MYSQL_DATABASE"  docker-compose.yml  | awk -F':' '{print $2}')
+MYSQL_DB_STRIP=`echo ${MYSQL_DB} | sed 's/ //g'`
+
+MYSQL_PASSWORD=$(grep "MYSQL_ROOT_PASSWORD"  docker-compose.yml  | awk -F':' '{print $2}')
+MYSQL_PASSWORD_STRIP=$(echo $MYSQL_PASSWORD | sed 's/ //g')
+
+echo "mysql database: $MYSQL_DB"
+echo "root password: $MYSQL_PASSWORD_STRIP"
+
+docker exec mysql mysql -uroot -p$MYSQL_PASSWORD_STRIP  $MYSQL_DB_STRIP < mysql/sql/v1.3.2_00.sql
+
 # init result verify
 [ $? -eq 0 ] && echo -e "AtomCI 初始化成功(:\n\n访问atomci: http://localhost:8090 \n" || echo -e "AtomCI 初始化失败, 请确认atomci 容器日志，\n或是 https://github.com/go-atomci/atomci-press/issues/new 反馈你的问题(:"
